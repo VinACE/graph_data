250075e31f5e8daa69ac878dbc456a331d573870
VinACE <vinsent.storagea@gmail.com>
2025-08-27 03:49:58 +0200

    Update graph_query.py

--- graph_query.py
+++ graph_query.py
@@ -1,60 +1,45 @@
 import json
-from typing import List, Dict
 
 class GraphQuery:
-    def __init__(self, json_file: str):
+    def __init__(self, json_file):
         with open(json_file, "r") as f:
             self.data = json.load(f)
 
-        # Normalize applications and functions for easier lookup
-        self.apps = {}
-        self.functions = {}
-        for app in self.data.get("applications", []):
-            app_name_norm = self._normalize(app["name"])
-            self.apps[app_name_norm] = {
-                "name": app["name"],
-                "functions": {}
-            }
-            for fn in app.get("functions", []):
-                fn_name_norm = self._normalize(fn["name"])
-                self.apps[app_name_norm]["functions"][fn_name_norm] = fn
-                self.functions[fn_name_norm] = self.functions.get(fn_name_norm, [])
-                self.functions[fn_name_norm].append(app["name"])
+    def list_apps(self):
+        return [app["name"] for app in self.data["applications"]]
 
-    def _normalize(self, name: str) -> str:
-        return name.strip().lower().replace(" ", "_").replace("-", "_")
+    def get_functions_for_app(self, app_name):
+        for app in self.data["applications"]:
+            if app["name"] == app_name:
+                return [fn["name"] for fn in app["functions"]]
+        return []
 
-    def list_apps(self) -> List[str]:
-        return [app["name"] for app in self.apps.values()]
+    def get_apps_for_function(self, fn_name):
+        apps = []
+        for app in self.data["applications"]:
+            for fn in app["functions"]:
+                if fn["name"] == fn_name:
+                    apps.append(app["name"])
+        return apps
 
-    def get_functions_for_app(self, app_name: str) -> List[str]:
-        app_name_norm = self._normalize(app_name)
-        app = self.apps.get(app_name_norm)
-        if not app:
-            return []
-        return [fn["name"] for fn in app["functions"].values()]
+    def get_variables_for_function(self, fn_name):
+        vars_set = set()
+        for app in self.data["applications"]:
+            for fn in app["functions"]:
+                if fn["name"] == fn_name:
+                    vars_set.update(fn.get("variables", []))
+        return list(vars_set)
 
-    def get_apps_for_function(self, fn_name: str) -> List[str]:
-        fn_name_norm = self._normalize(fn_name)
-        return self.functions.get(fn_name_norm, [])
+    def get_app_structure(self, app_name):
+        for app in self.data["applications"]:
+            if app["name"] == app_name:
+                return {fn["name"]: fn.get("variables", []) for fn in app["functions"]}
+        return {}
 
-    def get_variables_for_function(self, fn_name: str) -> List[str]:
-        fn_name_norm = self._normalize(fn_name)
-        apps = self.functions.get(fn_name_norm, [])
-        variables = []
-        for app_name in apps:
-            app_name_norm = self._normalize(app_name)
-            fn = self.apps[app_name_norm]["functions"].get(fn_name_norm)
-            if fn:
-                variables.extend(fn.get("variables", []))
-        return variables
-
-    def get_app_structure(self, app_name: str) -> Dict[str, List[str]]:
-        app_name_norm = self._normalize(app_name)
-        app = self.apps.get(app_name_norm)
-        if not app:
-            return {}
-        structure = {}
-        for fn in app["functions"].values():
-            structure[fn["name"]] = fn.get("variables", [])
-        return structure
+    def get_functions_for_variable(self, var_name):
+        funcs = []
+        for app in self.data["applications"]:
+            for fn in app["functions"]:
+                if var_name in fn.get("variables", []):
+                    funcs.append(fn["name"])
+        return funcs
